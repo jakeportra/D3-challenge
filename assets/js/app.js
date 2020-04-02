efad45b2126b94bb7b82da8fdf1914ddcc6efc91
Jake Portra <portr003@umn.edu>
2020-04-02 10:18:52 -0500

    added svg element

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,44 @@
-// @TODO: YOUR CODE HERE!
+// function that automatically resizes scatterplot
+function makeResponsive() {
+    
+    // make sure svg area is empty, clear it if not
+    var svgArea = d3.select("body").select("svg");
+
+    if (!svgArea.empty()) {
+        svgArea.remove();
+    }
+
+    // determine svg wrapper dimensions based on dimensions of browser window
+    var svgWidth = window.innerWidth;
+    var svgHeight = window.innerHeight;
+
+    var margin = {
+        top: 50,
+        bottom: 50,
+        right: 50,
+        left: 50
+    };
+
+    var height = svgHeight - margin.top - margin.bottom;
+    var width = svgWidth - margin.top - margin.bottom;
+
+    // add svg element
+    var svg = d3
+        .select(".chart")
+        .append("svg")
+        .attr("height", svgHeight)
+        .attr("width", svgWidth);
+
+    // add group element
+    var chartGroup = svg.append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // read csv
+    d3.csv("data.csv").then(function(stateData) {
+
+        // parse data
+
+        
+        
+    })
+}
